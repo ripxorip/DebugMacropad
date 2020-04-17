c76a821b0118f1d1ca0ee20c04c2fe4caf986d9b
Philip Karlsson <philip.karlsson@husqvarnagroup.com>
2020-04-17 15:54:47 +0200

    First working version

--- arduino/MacroPad/MacroPad.ino
+++ arduino/MacroPad/MacroPad.ino
@@ -1,4 +1,5 @@
-// digital pin 2 has a pushbutton attached to it. Give it a name:
+#include <Encoder.h>
+
 const int pullup_start_index = 2;
 const int pullup_end_index = pullup_start_index+5;
 const int normal_end_index = pullup_end_index+6;
@@ -8,13 +9,16 @@ const int num_pins = normal_end_index - pullup_start_index;
 static int state[num_pins];
 static int counter = 0;
 
+Encoder left_enc(6+2, 7+2);
+Encoder right_enc(9+2, 8+2);
+
 void send_report()
 {
     int index = 0;
     /* The json shall include a magic element for ID */
     /* Send function shall be on its own */
     /* Return a send whenever data is received.. */
-    Serial.print("{\"magic\": 0xb16b00b5,");
+    Serial.print("{\"magic\": 1337, ");
     for (int i = 0; i < num_pins-1; i++)
     {
         Serial.print("\"");
@@ -28,6 +32,12 @@ void send_report()
     Serial.print(index);
     Serial.print("\": ");
     Serial.print(state[index]);
+    Serial.print(",");
+    Serial.print("\"left_enc_count\":");
+    Serial.print(left_enc.read());
+    Serial.print(",");
+    Serial.print("\"right_enc_count\":");
+    Serial.print(right_enc.read());
     Serial.print("}");
     Serial.print("\n");
 }
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import time
 import glob
 import sys
 import serial
+import json
+import math
 
 
-def serial_ports():
+def serial_ports(filt):
     """ Lists serial port names
 
         :raises EnvironmentError:
@@ -24,43 +26,148 @@ def serial_ports():
         raise EnvironmentError('Unsupported platform')
 
     result = []
-    """
-    for port in ports:
+    filt_ports = []
+    for p in ports:
+        if filt in p:
+            filt_ports.append(p)
+
+    for port in filt_ports:
         try:
             s = serial.Serial(port, timeout=1)
             s.close()
             result.append(port)
         except (OSError, serial.SerialException):
             pass
-    """
-    return ports
+    return result
 
 class debug_macro_pad():
     def __init__(self):
-        pass
+        self.last_input = None
+        # Fix these maps
+        self.current_map = {
+            'func_out': 'a',
+            'step_in': 'F5',
+            'step_over': 'F5',
+            'reset': 'F5',
+            'run': 'F5',
+            'bp': 'F5',
+            'right_enc_count': 'F5',
+            'left_enc_count': 'F5',
+        }
+
+        self.enc_counts = {
+            'right_enc_count' : 0,
+            'left_enc_count': 0
+        }
+
+    def get_delta(self, inp):
+        if self.last_input == None:
+            self.last_input = inp
+            return inp
+        ret = {}
+        for i in inp:
+            if not inp[i] == self.last_input[i]:
+                ret[i] = inp[i]
+        return ret
+
+    def map_inputs(self, inp):
+        ret = {}
+        ret['func_out'] = inp['0']
+        ret['step_in'] = inp['1']
+        ret['step_over'] = inp['2']
+        ret['reset'] = inp['3']
+        ret['run'] = inp['4']
+        ret['bp'] = inp['10']
+        ret['left_enc_one'] = inp['6']
+        ret['left_enc_two'] = inp['7']
+        ret['right_enc_one'] = inp['8']
+        ret['right_enc_two'] = inp['9']
+        ret['right_enc_count'] = inp['right_enc_count']
+        ret['left_enc_count'] = inp['left_enc_count']
+        return ret
+
+    def handle_encoder_pin(self, key, value):
+        if 'enc_count' in key:
+            num_before = math.floor(self.enc_counts[key])
+            diff = value - self.last_input[key]
+            self.enc_counts[key] += diff/4.00
+            fract = self.enc_counts[key] - math.floor(self.enc_counts[key])
+            if fract == 0.00:
+                keyboard.press_and_release(self.current_map[key])
+
+
+    def handle_signal(self, key, value):
+        if 'enc' in key:
+            self.handle_encoder_pin(key, value)
+            return
+
+        # Handle normal keys
+        if value == 0:
+            keyboard.press(self.current_map[key])
+        else:
+            keyboard.release(self.current_map[key])
+
+    def process_input(self, inp):
+        inmap = self.map_inputs(inp)
+        delta = self.get_delta(inmap)
+
+        for d in delta:
+            self.handle_signal(d, delta[d])
+        self.last_input = inmap
+
+    def validate_packet(self, packet):
+        ret = False
+        try:
+            pp = json.loads(packet)
+            if pp['magic'] == 1337:
+                ret = True
+        except json.decoder.JSONDecodeError as e:
+            print('Got illegal packet: ' + packet)
+        return ret
 
     def open_port(self):
-        ret = False
-        ports = serial_ports()
+        ret = None
+        ports = serial_ports('tty.usbserial')
+        for p in ports:
+            lines = []
+            s = serial.Serial(p, 115200, timeout=1.5)
+            lines.append(s.readline().decode())
+            lines.append(s.readline().decode())
+            for l in lines:
+                if self.validate_packet(l):
+                    ret = s
+            if ret is not None:
+                break
+            s.close()
         return ret
 
     def communicate(self):
         # Will not return as long as
         # we have a valid connection
-        pass
+        print('Found keyboard; listening')
+        try:
+            last_pp = None
+            while True:
+                inp = self.port.readline()
+                if not self.validate_packet(inp):
+                    break
+                self.process_input(json.loads(inp))
+        except serial.serialutil.SerialException:
+            print('Serial port broken..')
 
     def run(self):
         while True:
-            if self.open_port():
+            port = self.open_port()
+            if port is not None:
+                self.port = port
                 self.communicate()
             # We got here because we could not find any port
             # or the communication stopped working
-
+            print('Communication lost searching for keyboard..')
             time.sleep(1)
 
 
 def main():
-    print(serial_ports())
     dmc = debug_macro_pad()
     dmc.run()
 
